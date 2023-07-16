2a18c24389648d7f1c89dea28835f57155c6992e
Kevin Feng <kevinfeng.cs88@gmail.com>
2023-07-16 08:13:53 -0400

    Update .wezterm.lua
    left and right padding removed

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,6 +2,29 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
+wezterm.on('user-var-changed', function(window, pane, name, value)
+    local overrides = window:get_config_overrides() or {}
+    if name == "ZEN_MODE" then
+        local incremental = value:find("+")
+        local number_value = tonumber(value)
+        if incremental ~= nil then
+            while (number_value > 0) do
+                window:perform_action(wezterm.action.IncreaseFontSize, pane)
+                number_value = number_value - 1
+            end
+            overrides.enable_tab_bar = false
+        elseif number_value < 0 then
+            window:perform_action(wezterm.action.ResetFontSize, pane)
+            overrides.font_size = nil
+            overrides.enable_tab_bar = true
+        else
+            overrides.font_size = number_value
+            overrides.enable_tab_bar = false
+        end
+    end
+    window:set_config_overrides(overrides)
+end)
+
 -- This table will hold the configuration.
 local config = {}
 
@@ -20,15 +43,21 @@ config.disable_default_key_bindings = true
 config.window_background_opacity = 0.9
 -- config.window_background_image = 'C:/Users/Kevin/space.gif'
 config.window_background_image_hsb = {
-  -- Darken the background image by reducing it to 1/3rd
-  brightness = 0.1,
+    -- Darken the background image by reducing it to 1/3rd
+    brightness = 0.1,
 
-  -- You can adjust the hue by scaling its value.
-  -- a multiplier of 1.0 leaves the value unchanged.
-  hue = 1.0,
+    -- You can adjust the hue by scaling its value.
+    -- a multiplier of 1.0 leaves the value unchanged.
+    hue = 1.0,
 
-  -- You can adjust the saturation also.
-  saturation = 1.0,
+    -- You can adjust the saturation also.
+    saturation = 1.0,
+}
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
 }
 
 config.unix_domains = {
